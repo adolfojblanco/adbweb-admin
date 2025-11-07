f71fcfadb9650526ffe3251b093cee4f291b1c78
Adolfo Blanco <ajb156@outlook.com>
2025-11-07 06:45:04 +0100

    update

--- backend/adbwebdesign/urls.py
+++ backend/adbwebdesign/urls.py
@@ -2,7 +2,7 @@
 URL configuration for adbwebdesign project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework_simplejwt.views import (
@@ -24,11 +24,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    ## APPS ROUTES
-    path("api/v1/catalog/", include(("apps.catalogs.urls", "catalog"), namespace="v1-catalog")),
-    ## APPS ROUTES
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
